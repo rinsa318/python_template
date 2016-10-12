1d7a06fba18c28e415fccaccf340f8c2f7746629
wazano318 <n1.n2n3__n4n5@ruri.waseda.jp>
2016-10-11 20:41:13 -0400

    add "addition" + "multiplication"

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 print("I'm studying about git by using SourceTree")
 print("I'm studying about git by using SourceTree2")
 
-a = 0
+a = 1
+b = 1
 for i in range(100):
 
-	print(a)
+	print("addition" + str(a))
+
 	a += i
+
+	print("multiplication" + str(b))
+
+	if i==0:
+		continue
+	
+	else:
+		b *= i
