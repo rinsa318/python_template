46e52ad38b7a8eb397784270949ea78f41299baa
wazano318 <n1.n2n3__n4n5@ruri.waseda.jp>
2016-10-11 22:57:07 -0400

    make plot

--- test.py
+++ test.py
@@ -1,18 +1,43 @@
-print("I'm studying about git by using SourceTree")
-print("I'm studying about git by using SourceTree2")
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
 
-a = 1
-b = 1
-for i in range(100):
 
-	print("addition" + str(a))
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-	a += i
+x = y = np.arange(-3, 3, 0.1)
+X, Y = np.meshgrid(x, y)
 
-	print("multiplication" + str(b))
+sigma = 2
+#Z = np.exp(-(X**2 + Y**2)/(2*sigma**2)) / (2*np.pi*sigma**2)
+#Z = (-(X**2 + Y**2))
+Z = -(X + Y)**2
 
-	if i==0:
-		continue
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm)
+# ax.plot_wireframe(X, Y, Z, rstride=2, cstride=2)
+# ax.scatter(X, Y, Z, s=1)
+
+plt.show()
+
+# print("I'm studying about git by using SourceTree")
+# print("I'm studying about git by using SourceTree2")
+
+# a = 1
+# b = 1
+# for i in range(100):
+
+# 	print("addition" + str(a))
+
+# 	a += i
+
+# 	print("multiplication" + str(b))
+
+# 	if i==0:
+# 		continue
 	
-	else:
-		b *= i
+# 	else:
+# 		b *= i
+
+
